d06177d867fbe10b56a0a4b96953b31aeea3a3d3
b-reddington <brandon.reddington98@gmail.com>
2023-04-28 01:21:26 -0700

    Added styling for the login/registration pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -79,4 +79,9 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+router.get('/signup', (req, res) => {
+    res.render('signup');
+    return;
+});
+
 module.exports = router;
