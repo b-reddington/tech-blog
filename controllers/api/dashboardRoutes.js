1ceed74d52fe3dc3ca23938ecfc8e6ec684fa203
b-reddington <brandon.reddington98@gmail.com>
2023-04-28 02:30:18 -0700

    Login and Registration fully functional

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -2,4 +2,6 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// Dashboard should contain the posts of the logged in user
+
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -42,6 +42,32 @@ router.post('/login', async (req, res) => {
     }
 })
 
+router.post('/signup', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        console.log(password)
+        if (password.length < 8) {
+            throw new Error('Password is too short')
+        }
+        const saltRounds = 10; // Number of salt rounds to use for the hash
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+        const userData = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword // Store the hashed password in the database
+        });
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json({ userData, message: 'User created successfully' });
+        });
+
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
 router.post('/logout', (req, res) => {
     if (req.session.logged_in) {
         req.session.destroy(() => {
