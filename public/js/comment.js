f842580cc7de79b6ab34a6e3ec014849b0f9f986
b-reddington <brandon.reddington98@gmail.com>
2023-05-01 02:03:21 -0700

    Posts in home and dashboard are now sorted by date created

---
+++ public/js/comment.js
@@ -0,0 +1,25 @@
+const newComment = async (event) => {
+    event.preventDefault();
+
+    const comment = document.querySelector('#comment').value.trim();
+
+    if (comment) {
+        const response = await fetch(`/api/comments`, {
+            method: 'POST',
+            body: JSON.stringify({ comment }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            location.reload();
+        } else {
+            alert('Failed to create post');
+        }
+    }
+};
+
+document
+    .querySelector('.new-comment-form')
+    .addEventListener('submit', newComment);
