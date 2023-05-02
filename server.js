6d27ec744bb442a5522c1b6703835313e6bc7b54
b-reddington <brandon.reddington98@gmail.com>
2023-05-01 23:41:23 -0700

    Push to heroku

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ const sess = {
     secret: 'Super secret secret',
     cookie: {
         maxAge: 24 * 60 * 60 * 1000,
-        httpOnly: false,
-        secure: true,
-        sameSite: 'strict',
+        // httpOnly: false,
+        // secure: true,
+        // sameSite: 'strict',
     },
     resave: false,
     saveUninitialized: true,
